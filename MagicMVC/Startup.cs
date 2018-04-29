b28835612dc8a5282c2acdd06e00d9baaad7af5c
Antman <antfellow@gmail.com>
2018-04-29 22:36:20 +1000

    Merged together auth and master

--- MagicMVC/Startup.cs
+++ MagicMVC/Startup.cs
@@ -26,6 +26,9 @@ namespace MagicMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MagicMVCContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
