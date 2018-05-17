31cdc33a77a38ad9f71a843c78193de14291354c
Antman <antfellow@gmail.com>
2018-05-17 13:50:22 +1000

    Added basic shopping cart functionality

--- MagicMVC/Data/MagicMVCContext.cs
+++ MagicMVC/Data/MagicMVCContext.cs
@@ -9,6 +9,8 @@ namespace MagicMVC.Models
         public DbSet<Store> Stores { get; set; }
         public DbSet<OwnerInventory> OwnerInventory { get; set; }
         public DbSet<StockRequest> StockRequests { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
 
         public MagicMVCContext(DbContextOptions<MagicMVCContext> options) : base(options)
         { }
--- MagicMVC/Models/Purchase.cs
+++ MagicMVC/Models/Purchase.cs
@@ -1,14 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace MagicMVC.Models
 {
     public class Purchase
     {
+        public int PurchaseID { get; set; }
         public int StoreID { get; set; }
         public int ProductID { get; set; }
         public int QuantityToPurchase { get; set; }
+        public String CustomerID { get; set; }
+        public Boolean Confirmed { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+
     }
 }
 
